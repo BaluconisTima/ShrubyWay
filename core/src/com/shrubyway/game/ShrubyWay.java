686c989752440c3e823aaafdc3375c2f46637f3f
root <root@DESKTOP-NRLK271.localdomain>
2023-04-05 13:20:17 +0200

    Initial commit

---
+++ core/src/com/shrubyway/game/ShrubyWay.java
@@ -0,0 +1,24 @@
+package com.shrubyway.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class ShrubyWay extends ApplicationAdapter {
+	
+	@Override
+	public void create () {
+
+	}
+
+	@Override
+	public void render () {
+
+	}
+
+	@Override
+	public void dispose () {
+
+	}
+}
---
+++ desktop/src/com/shrubyway/game/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.shrubyway.game;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.shrubyway.game.ShrubyWay;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("ShrubyWay");
+		new Lwjgl3Application(new ShrubyWay(), config);
+	}
+}
