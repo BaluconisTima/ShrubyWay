a2c3d04a1ec94f44a63f1886f7b946bf24a38916
Tsimafei Baliukonis <t3783261@gmail.com>
2023-04-05 23:17:12 +0200

    The character is attached to the camera, corrected animation errors.

--- core/src/com/shrubyway/game/Shraby.java
+++ core/src/com/shrubyway/game/Shraby.java
@@ -8,25 +8,30 @@ import com.badlogic.gdx.graphics.g2d.Batch;
 public class Shraby {
     private final Vector2 position = new Vector2();
     private byte FaceDirection = 0;  // 0 - DOWN, 1 - UP, 2 - LEFT, 3 - RIGHT
+    private boolean IsMoving = false;
     private byte lastFaceDirection = 0;
-    private final int Speed = 10;
+    private boolean lastIsMoving = false;
+
+    private final int Speed = 7;
     Animation<TextureRegion> CurrentAnimation;
     float AnimationStateTime;
-    private Texture AnimationList = new Texture(Gdx.files.internal("SHRABY_AFK_DOWN.png"));
+
+    private Texture AnimationList = new Texture(Gdx.files.internal("SHRABY/AFK/DOWN.png"));
+    Animator animator = new Animator();
     TextureRegion currentFrame;
     public Shraby(float x, float y) {
       position.set(x, y);
-      Animator animator = new Animator();
-      CurrentAnimation = animator.toAnimation(AnimationList, 1, 30);
-      AnimationStateTime = 0f;
-
-
+        AnimationStateTime = 0f;
+        CurrentAnimation = animator.toAnimation(AnimationList, 30);
     }
 
     public void render(Batch batch) {
-           if(FaceDirection != lastFaceDirection) {
+           if(FaceDirection != lastFaceDirection || IsMoving != lastIsMoving) {
                lastFaceDirection = FaceDirection;
-               String s = "SHRABY_AFK_";
+               lastIsMoving = IsMoving;
+               String s = "SHRABY/";
+               if(IsMoving) s += "WALK/";
+               else s += "AFK/";
                switch(FaceDirection) {
                    case 0: s += "DOWN"; break;
                    case 1: s += "UP"; break;
@@ -35,9 +40,9 @@ public class Shraby {
                }
                s += ".png";
                AnimationList = new Texture(Gdx.files.internal(s));
-               Animator animator = new Animator();
-               CurrentAnimation = animator.toAnimation(AnimationList, 1, 30);
                AnimationStateTime = 0f;
+               CurrentAnimation = animator.toAnimation(AnimationList, 30);
+
            }
            AnimationStateTime += Gdx.graphics.getDeltaTime();
            currentFrame = CurrentAnimation.getKeyFrame(AnimationStateTime, true);
@@ -46,11 +51,16 @@ public class Shraby {
                    position.y - currentFrame.getRegionHeight() / 2);
     }
     public void moveTo(Vector2 direction) {
+        if(direction.x == 0 && direction.y == 0) IsMoving = false;
+        else IsMoving = true;
         if(direction.y < 0) FaceDirection = 0;
         else if(direction.y > 0) FaceDirection = 1;
         else if(direction.x < 0) FaceDirection = 2;
         else if(direction.x > 0) FaceDirection = 3;
         position.add(direction.scl(Speed));
+    }
+    public Vector2 Position() {
+        return position;
     }
 
     public void dispose() {
--- core/src/com/shrubyway/game/ShrubyWay.java
+++ core/src/com/shrubyway/game/ShrubyWay.java
@@ -3,10 +3,13 @@ package com.shrubyway.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Graphics;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Interpolation;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.graphics.g2d.Batch;
 
@@ -15,32 +18,45 @@ public class ShrubyWay extends ApplicationAdapter {
 	private Shraby player;
 	SpriteBatch batch;
 	OrthographicCamera Camera;
+	Vector2 CameraPosition;
+	Texture TEST_OBJECT;
 
 	private KeyboardAdapter InputProcessor = new KeyboardAdapter();
 
 	@Override
 	public void create () {
+		TEST_OBJECT = new Texture(Gdx.files.internal("Test.png"));
 		Gdx.graphics.setWindowedMode(1920, 1080);
+		CameraPosition = new Vector2(Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2);
 		Graphics.DisplayMode currentDisplayMode = Gdx.graphics.getDisplayMode();
 		Gdx.graphics.setFullscreenMode(currentDisplayMode);
 		Camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-		Camera.position.set(Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2, 0);
+		Camera.position.set(CameraPosition.x,CameraPosition.y, 0);
 
 
 		Gdx.input.setInputProcessor(InputProcessor);
+		batch = new SpriteBatch();
+		player = new Shraby(Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2);
+		//Gdx.graphics.setVSync(true);
 
-         player = new Shraby(Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2);
-		 batch = new SpriteBatch();
 
 	}
 
 	@Override
 	public void render () {
 		ScreenUtils.clear(1,1,1,1);
+		Vector2 temp = player.Position();
+		if(Math.abs((temp.x - CameraPosition.x)) > Gdx.graphics.getWidth() / 4)
+			temp.x -= Gdx.graphics.getWidth() / 4 + CameraPosition.x;
+
+		CameraPosition.lerp(new Vector2(temp.x, temp.y), 0.1f);
+		Camera.position.set(CameraPosition.x,CameraPosition.y, 0);
 		Camera.update();
 		batch.setProjectionMatrix(Camera.combined);
         player.moveTo(InputProcessor.getMovementDirection());
+
 		batch.begin();
+		batch.draw(TEST_OBJECT, 0, 0);
 		player.render(batch);
 		batch.end();
 
