53b51208cd54f475a4a1e90cf7057b9b6ea138f2
Tsimafei Baliukonis <t3783261@gmail.com>
2023-04-05 19:28:40 +0200

    moving the character by WASD, opening in full screen mode, animation

--- core/src/com/shrubyway/game/Shraby.java
+++ core/src/com/shrubyway/game/Shraby.java
@@ -7,22 +7,38 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
 public class Shraby {
     private final Vector2 position = new Vector2();
-    private final byte FaceDirection = 0;  // 0 - DOWN, 1 - UP, 2 - LEFT, 3 - RIGHT
+    private byte FaceDirection = 0;  // 0 - DOWN, 1 - UP, 2 - LEFT, 3 - RIGHT
+    private byte lastFaceDirection = 0;
     private final int Speed = 10;
     Animation<TextureRegion> CurrentAnimation;
     float AnimationStateTime;
-    private final Texture AFK_DOWN_texture = new Texture(Gdx.files.internal("Shraby_AFK_down.png"));
+    private Texture AnimationList = new Texture(Gdx.files.internal("SHRABY_AFK_DOWN.png"));
     TextureRegion currentFrame;
     public Shraby(float x, float y) {
       position.set(x, y);
       Animator animator = new Animator();
-      CurrentAnimation = animator.toAnimation(AFK_DOWN_texture, 1, 30);
-      //AnimationStateTime = 0f;
+      CurrentAnimation = animator.toAnimation(AnimationList, 1, 30);
+      AnimationStateTime = 0f;
 
 
     }
 
     public void render(Batch batch) {
+           if(FaceDirection != lastFaceDirection) {
+               lastFaceDirection = FaceDirection;
+               String s = "SHRABY_AFK_";
+               switch(FaceDirection) {
+                   case 0: s += "DOWN"; break;
+                   case 1: s += "UP"; break;
+                   case 2: s += "LEFT"; break;
+                   case 3: s += "RIGHT"; break;
+               }
+               s += ".png";
+               AnimationList = new Texture(Gdx.files.internal(s));
+               Animator animator = new Animator();
+               CurrentAnimation = animator.toAnimation(AnimationList, 1, 30);
+               AnimationStateTime = 0f;
+           }
            AnimationStateTime += Gdx.graphics.getDeltaTime();
            currentFrame = CurrentAnimation.getKeyFrame(AnimationStateTime, true);
            batch.draw(currentFrame,
@@ -30,6 +46,10 @@ public class Shraby {
                    position.y - currentFrame.getRegionHeight() / 2);
     }
     public void moveTo(Vector2 direction) {
+        if(direction.y < 0) FaceDirection = 0;
+        else if(direction.y > 0) FaceDirection = 1;
+        else if(direction.x < 0) FaceDirection = 2;
+        else if(direction.x > 0) FaceDirection = 3;
         position.add(direction.scl(Speed));
     }
 
