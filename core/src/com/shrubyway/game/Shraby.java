b5db3b54b037e7d466965b0d541c5eb994d04390
Tsimafei Baliukonis <t3783261@gmail.com>
2023-04-05 18:27:35 +0200

    moving the character by WASD, opening in full screen mode, animation

--- core/src/com/shrubyway/game/Shraby.java
+++ core/src/com/shrubyway/game/Shraby.java
@@ -1,29 +1,40 @@
 package com.shrubyway.game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
 public class Shraby {
     private final Vector2 position = new Vector2();
-    private final byte FaceDirection = 0;
-    private final int Speed = 5;
-    private final Texture texture;
-
+    private final byte FaceDirection = 0;  // 0 - DOWN, 1 - UP, 2 - LEFT, 3 - RIGHT
+    private final int Speed = 10;
+    Animation<TextureRegion> CurrentAnimation;
+    float AnimationStateTime;
+    private final Texture AFK_DOWN_texture = new Texture(Gdx.files.internal("Shraby_AFK_down.png"));
+    TextureRegion currentFrame;
     public Shraby(float x, float y) {
-      texture = new Texture("Test.png");
       position.set(x, y);
+      Animator animator = new Animator();
+      CurrentAnimation = animator.toAnimation(AFK_DOWN_texture, 1, 30);
+      //AnimationStateTime = 0f;
+
+
     }
 
     public void render(Batch batch) {
-           batch.draw(texture,
-                   position.x - texture.getWidth() / 2,
-                   position.y - texture.getHeight() / 2);
+           AnimationStateTime += Gdx.graphics.getDeltaTime();
+           currentFrame = CurrentAnimation.getKeyFrame(AnimationStateTime, true);
+           batch.draw(currentFrame,
+                   position.x - currentFrame.getRegionWidth() / 2,
+                   position.y - currentFrame.getRegionHeight() / 2);
     }
     public void moveTo(Vector2 direction) {
         position.add(direction.scl(Speed));
     }
 
     public void dispose() {
-        texture.dispose();
+      //  currentFrame.dispose();
     }
 
 }
